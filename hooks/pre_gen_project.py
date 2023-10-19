eaf0694f86298c46140ca3bc075062e930686969
Ryzal Kamis <ryzalk.co@gmail.com>
2023-10-19 15:00:16 +0800

    Initial commit.

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,78 @@
+import re
+import sys
+
+
+COOKIE_INPUTS = {
+    "project_name": {
+        "user_input": "{{cookiecutter.project_name}}",
+        "regex": r"^[a-zA-Z_]+(?:_? [a-zA-Z0-9]+)*$"},
+    "description": {
+        "user_input": "{{cookiecutter.description}}"},
+    "repo_name": {
+        "user_input": "{{cookiecutter.repo_name}}",
+        "regex": r"^[a-z][a-z0-9-]+$"},
+    "src_package_name": {
+        "user_input": "{{cookiecutter.src_package_name}}",
+        "regex": r"^[a-z](?:_?[a-z0-9]+)*$"},
+    "src_package_name_short": {
+        "user_input": "{{cookiecutter.src_package_name_short}}",
+        "regex": r"^[a-z](?:_?[a-z0-9]+)*$"},
+    "harbor_registry_project_path": {
+        "user_input": "{{cookiecutter.harbor_registry_project_path}}",
+        "regex": r"^registry\.aisingapore\.net[\-\/\w\d]+(?:[a-z0-9]+)$"},
+    "author_name": {
+        "user_input": "{{cookiecutter.author_name}}",
+        "regex": r"^[a-zA-Z](?:_?[a-zA-Z0-9]+)*$"}
+}
+
+ERROR_MSG_LIST = []
+
+
+def check_input_length(cookie_input_key, cookie_input_val):
+
+    input_val = cookie_input_val["user_input"].strip()
+    if len(input_val) not in range(1, 73):
+        ERROR_MSG_LIST.append("ERROR: %s - '%s' is not of valid length (1 to 72)."
+            % (cookie_input_key, cookie_input_val["user_input"]))
+
+
+def check_input_regex(cookie_input_key, cookie_input_val):
+
+    if not re.match(cookie_input_val["regex"], cookie_input_val["user_input"]):
+
+        if cookie_input_key == "project_name":
+            ERROR_MSG_LIST.append("ERROR: %s - '%s' is not a valid project name. Please use only alphanumeric characters."
+                % (cookie_input_key, cookie_input_val["user_input"]))
+
+        if cookie_input_key == "repo_name":
+            ERROR_MSG_LIST.append("ERROR: %s - '%s' is not a valid repository name. Only alphanumeric characters and hyphens are permitted."
+                % (cookie_input_key, cookie_input_val["user_input"]))
+
+        if cookie_input_key in ["src_package_name", "src_package_name_short"]:
+            ERROR_MSG_LIST.append("ERROR: %s - '%s' is not a valid Python package name."
+                % (cookie_input_key, cookie_input_val["user_input"]))
+
+        if cookie_input_key == "harbor_registry_project_path":
+            ERROR_MSG_LIST.append("ERROR: %s - '%s' is not a valid Harbor path."
+                % (cookie_input_key, cookie_input_val["user_input"]))
+
+        if cookie_input_key == "author_name":
+            ERROR_MSG_LIST.append("ERROR: %s - '%s' is not a valid name."
+                % (cookie_input_key, cookie_input_val["user_input"]))
+
+
+def check_cookiecutter_inputs():
+
+    for cookie_input_key, cookie_input_val in COOKIE_INPUTS.items():
+
+        check_input_length(cookie_input_key, cookie_input_val)
+        if "regex" in cookie_input_val:
+            check_input_regex(cookie_input_key, cookie_input_val)
+
+    if len(ERROR_MSG_LIST) > 0:
+
+        for error_msg in ERROR_MSG_LIST:
+            print(error_msg)
+        sys.exit(1)
+
+check_cookiecutter_inputs()
